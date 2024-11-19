3904c75dd4c1bd0a6db7c9b09d927e40e9d1851b
박가희 <930_10@naver.com>
2024-11-19 18:00:02 +0900

    Add: Search Component

--- final-pjt-front/src/components/Common/Search.vue
+++ final-pjt-front/src/components/Common/Search.vue
@@ -1,7 +1,41 @@
 <template>
-  <div>Search</div>
+  <form class="search-form" @submit.prevent="handleSearch">
+    <input
+      type="text"
+      v-model="searchInput"
+      placeholder="검색어를 입력하세요."
+      class="search-input"
+    />
+    <button @click="handleDelete">
+      <img src="@/assets/icons/x_btn.svg" alt="검색어 삭제" />
+    </button>
+    <button type="submit" aria-label="search">
+      <img src="@/assets/icons/search.svg" alt="검색" />
+    </button>
+  </form>
 </template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+  searchPath: {
+    type: String,
+    required: true,
+  },
+});
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const searchInput = ref("");
+const router = useRouter();
+const handleSearch = () => {
+  // 검색 버튼 눌렀을 때 향하는 경로
+  const pathName = props.searchPath;
+  router.push({ name: `${pathName}`, query: { search: searchInput.value } });
+};
+
+const handleDelete = () => {
+  searchInput.value = "";
+};
+</script>
 
 <style scoped></style>
