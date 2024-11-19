bb08ef9950f78c55d087c4d8e6852964a0a1c58b
박가희 <930_10@naver.com>
2024-11-19 18:02:40 +0900

    Feat: 회원가입 진행중

--- final-pjt-front/src/views/MainView.vue
+++ final-pjt-front/src/views/MainView.vue
@@ -2,12 +2,18 @@
   <div class="main">
     <h1 class="title">Main</h1>
   </div>
+  <RouterView />
 </template>
 
-<script setup></script>
+<script setup>
+import Search from "@/components/Common/Search.vue";
+import Menubar from "@/components/Header/Menubar.vue";
+import Navbar from "@/components/Header/Navbar.vue";
+import { useRouter } from "vue-router";
+</script>
 
 <style scoped>
-/* .main {
+.main {
   box-shadow: inset 0 0 20px red;
-} */
+}
 </style>
--- final-pjt-front/src/views/SignupView.vue
+++ final-pjt-front/src/views/SignupView.vue
@@ -1,7 +1,59 @@
 <template>
-  <div>SignupView</div>
+  <div>
+    <h1>회원가입</h1>
+    <form @submit.prevent="handleSignup">
+      <label for="email">이메일</label>
+      <input type="text" id="email" v-model="emailInput" class="user-input" />
+      <label for="username">이름</label>
+      <input type="text" id="username" v-model="nameInput" class="user-input" />
+      <label for="password1">비밀번호</label>
+      <input
+        type="password"
+        id="password1"
+        v-model="firstPwInput"
+        class="user-input"
+      />
+      <label for="password2">비밀번호 확인</label>
+      <input
+        type="password"
+        id="password2"
+        v-model="secondPwInput"
+        class="user-input"
+      />
+      <Button content="회원가입" :onClick="handleSignup" ariaLabel="회원가입" />
+    </form>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Button from "@/components/Common/Button.vue";
+import axios from "axios";
+import { ref } from "vue";
+
+const emailInput = ref("");
+const nameInput = ref("");
+const firstPwInput = ref("");
+const secondPwInput = ref("");
+
+const handleSignup = async () => {
+  try {
+    const userData = {
+      email: emailInput.value,
+      username: nameInput.value,
+      password1: firstPwInput.value,
+      password2: secondPwInput.value,
+    };
+    const response = axios.post(
+      `${import.meta.env.VITE_BASE_URL}/accounts/signup/`,
+      userData
+    );
+
+    console.log(response.data);
+  } catch (err) {
+    console.log(err.response.data);
+    throw new Error(err.response.data);
+  }
+};
+</script>
 
 <style scoped></style>
