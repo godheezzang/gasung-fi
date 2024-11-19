746fb75abe290633ea9d1acc012a27cb2b33af6f
박가희 <930_10@naver.com>
2024-11-19 18:00:15 +0900

    Add: Button Component

--- final-pjt-front/src/components/Common/Button.vue
+++ final-pjt-front/src/components/Common/Button.vue
@@ -1,7 +1,44 @@
 <template>
-  <div>Button</div>
+  <button
+    @click="handleClick"
+    :aria-label="ariaLabel"
+    :class="['btn', 'default-btn', customClass]"
+  >
+    {{ content }}
+  </button>
 </template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+  content: String,
+  onClick: Function,
+  ariaLabel: String,
+  customClass: String,
+});
 
-<style scoped></style>
+const handleClick = () => {
+  if (props.onClick) {
+    props.onClick();
+  }
+};
+
+console.log(props.customClass);
+</script>
+
+<style scoped>
+.default-btn {
+  color: var(--color-white);
+  background-color: var(--color-black);
+}
+
+.disabled-btn {
+  background-color: var(--color-gray-04);
+  color: var(--color-gray-01);
+}
+
+.small-btn {
+  font-size: var(--font-size-extra-small);
+  padding: 0.5rem 1rem;
+  border-radius: 0.8rem;
+}
+</style>
