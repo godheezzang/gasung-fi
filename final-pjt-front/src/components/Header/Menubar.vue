3b008549f8fb53e27828ffa837c56d44cb4d336d
박가희 <930_10@naver.com>
2024-11-19 18:00:33 +0900

    Add: navigation bar

--- final-pjt-front/src/components/Header/Menubar.vue
+++ final-pjt-front/src/components/Header/Menubar.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>Menubar</div>
+  <div>
+    <nav>
+      <RouterLink :to="{ name: 'home' }">메인</RouterLink>
+      <RouterLink :to="{ name: 'products' }">상품</RouterLink>
+      <RouterLink :to="{ name: 'exchange_rate' }">환율</RouterLink>
+      <RouterLink :to="{ name: 'search_bank' }">은행 검색</RouterLink>
+      <RouterLink :to="{ name: 'community' }">커뮤니티</RouterLink>
+    </nav>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
 
 <style scoped></style>
--- final-pjt-front/src/components/Header/Navbar.vue
+++ final-pjt-front/src/components/Header/Navbar.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>Navbar</div>
+  <div v-if="!isLoggedIn">
+    <RouterLink :to="{ name: 'login' }">로그인</RouterLink>
+    <RouterLink :to="{ name: 'signup' }">회원가입</RouterLink>
+  </div>
+  <div v-if="isLoggedIn">
+    <RouterLink :to="{ name: 'profile' }">프로필</RouterLink>
+    <RouterLink :to="{ name: 'my_products' }">내 상품</RouterLink>
+    <Button
+      :onClick="handleLogout"
+      content="로그아웃"
+      ariaLabel="로그아웃"
+      customClass="small-btn"
+    ></Button>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Button from "@/components/Common/Button.vue";
+import { useUserStore } from "@/stores/user";
+import { computed } from "vue";
+import { RouterLink } from "vue-router";
+
+const userStore = useUserStore();
+const isLoggedIn = computed(() => userStore.$state.isLoggedIn);
+
+const handleLogout = () => {
+  userStore.logout();
+};
+</script>
 
 <style scoped></style>
