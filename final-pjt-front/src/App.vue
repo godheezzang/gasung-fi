51cf6325d8f4281485aa1de98b49ece82cc487c0
박가희 <930_10@naver.com>
2024-11-19 17:59:40 +0900

    Style: css reset & layout

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import "@/assets/main.css";
+import Navbar from "@/components/Header/Navbar.vue";
+import Menubar from "@/components/Header/Menubar.vue";
 </script>
 
 <template>
   <!-- <RouterLink :to="{ name: 'home' }">main</RouterLink> -->
+
   <div class="container">
-    <RouterView />
+    <nav>
+      <Navbar />
+      <Menubar />
+    </nav>
+    <main>
+      <RouterView />
+    </main>
   </div>
 </template>
