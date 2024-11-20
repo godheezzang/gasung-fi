78bb13a1c01b9aa58712f5f0a9be873371ace9c1
hjkim2040 <hjkim2661@gmail.com>
2024-11-20 23:21:19 +0900

    Modify: 게시글 조회 시 댓글도 반환되도록 수정

--- final-pjt-back/articles/serializers.py
+++ final-pjt-back/articles/serializers.py
@@ -6,8 +6,15 @@ class ArticleCreateSerializer(serializers.ModelSerializer):
         model = Article
         fields = ('title', 'content',)
         read_only_fields = ('user',)
+class ArticleCommentsSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model = Comment
+        fields = ('id', 'user', 'content', 'main_comment', )
+
 
 class ArticleListSerializer(serializers.ModelSerializer):
+    comments = ArticleCommentsSerializer(source="comment_set", many=True)
     user = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Article
