05520ac10def26b8668ed2c574540cb15a453d69
김성찬 <hjkim2661@gmail.com>
2024-11-22 11:20:08 +0900

    Modify: .idea 삭제

--- final-pjt-back/exchange_rate/views.py
+++ final-pjt-back/exchange_rate/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 import requests
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view,permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
 from gasung_fi import my_settings
 from datetime import datetime, timedelta
 # Create your views here.
