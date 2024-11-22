51a7cdf5e152d3ea933b80311ccc47d1774dc613
김성찬 <hjkim2661@gmail.com>
2024-11-22 11:52:57 +0900

    Modify: 게시글, 댓글 기능 수정

--- final-pjt-back/articles/serializers.py
+++ final-pjt-back/articles/serializers.py
@@ -4,29 +4,36 @@ from .models import Article, Comment
 class ArticleCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ('title', 'content', "id")
+        fields = ('title', 'content',)
         read_only_fields = ('user',)
 class ArticleCommentsSerializer(serializers.ModelSerializer):
+    comment_id = serializers.IntegerField(source="id", read_only=True)
     user = serializers.StringRelatedField(read_only=True)
+    replies = serializers.SerializerMethodField()
     class Meta:
         model = Comment
-        fields = ('id', 'user', 'content', )
+        fields = ('user', 'content', "comment_id", 'replies',)
+
+    def get_replies(self, obj):
+        replies = Comment.objects.filter(main_comment=obj)
+        return ArticleCommentsSerializer(replies, many=True).data
 
 
 class ArticleListSerializer(serializers.ModelSerializer):
+    article_id = serializers.IntegerField(source="id", read_only=True)
     comments = serializers.SerializerMethodField()
     user = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Article
-        fields = '__all__'
+        exclude = ("id",)
 
     def get_comments(self, obj):
-        comments = Comment.objects.filter(main_comment__isnull=True)
+        comments = Comment.objects.filter(article = obj, main_comment__isnull=True)
         return ArticleCommentsSerializer(comments, many=True).data
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ('content', "id")
+        fields = ("id",'content',)
         read_only_fields = ('user', 'article', 'main_comment',)
