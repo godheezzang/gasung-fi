ba1ffd36a9a805464257388066fed66feab9e72a
박가희 <930_10@naver.com>
2024-11-23 23:21:13 +0900

    Refactor: 로그인 & 회원가입 로직 수정

--- final-pjt-front/src/stores/user.js
+++ final-pjt-front/src/stores/user.js
@@ -1,46 +1,103 @@
+import axios from "axios";
 import { defineStore } from "pinia";
 import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
 
 export const useUserStore = defineStore("user", () => {
-  const user = ref(null);
   const isLoggedIn = ref(false);
   const token = ref(null);
   const userEmail = ref(null);
+  const username = ref(null);
+  const router = useRouter();
 
   onMounted(() => {
     const storedIsLoggedIn = JSON.parse(sessionStorage.getItem("isLoggedIn"));
     const storedToken = JSON.parse(sessionStorage.getItem("userToken"));
     const storedEmail = JSON.parse(sessionStorage.getItem("userEmail"));
-    // const storedUsername = JSON.parse(sessionStorage.getItem("username"));
+    const storedUsername = JSON.parse(sessionStorage.getItem("username"));
 
     console.log(storedEmail);
     if (storedIsLoggedIn && storedToken) {
       isLoggedIn.value = true;
       token.value = storedToken;
       userEmail.value = storedEmail;
-      // username.value = storedUsername;
+      username.value = storedUsername;
     }
   });
 
-  const login = (userData, userToken) => {
-    user.value = userData;
-    token.value = userToken;
-    isLoggedIn.value = true;
+  const signup = async (data) => {
+    const username = data.username;
+    const email = data.email;
+    const password1 = data.password1;
+    const password2 = data.password2;
 
-    sessionStorage.setItem("userToken", JSON.stringify(token.value));
-    sessionStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn.value));
-    sessionStorage.setItem("userEmail", JSON.stringify(user.value.email));
-    // sessionStorage.setItem("username", JSON.stringify(user.value.username));
+    try {
+      const response = await axios({
+        method: "post",
+        url: `${import.meta.env.VITE_BASE_URL}/accounts/signup/`,
+        data: {
+          username,
+          email,
+          password1,
+          password2,
+        },
+      });
+
+      // console.log(response);
+      if (response.status === 201) {
+        const userData = data;
+        userData.userToken = response.data.key;
+
+        // console.log(userData);
+        login(userData);
+      }
+    } catch (error) {
+      console.error(error);
+      return error.response ? error.response.data : "알 수 없는 오류가 발생했습니다.";
+    }
+  };
+
+  const login = async (data) => {
+    try {
+      const response = await axios({
+        method: "post",
+        url: `${import.meta.env.VITE_BASE_URL}/accounts/login/`,
+        data: {
+          email: data.email,
+          password: data.password ? data.password : data.password1,
+        },
+      });
+
+      // console.log(response);
+
+      userEmail.value = data.email;
+      token.value = response.data.key;
+      username.value = response.data.username;
+      isLoggedIn.value = true;
+
+      sessionStorage.setItem("userToken", JSON.stringify(token.value));
+      sessionStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn.value));
+      sessionStorage.setItem("userEmail", JSON.stringify(userEmail.value));
+      sessionStorage.setItem("username", JSON.stringify(username.value));
+
+      router.push({ name: "home" });
+    } catch (error) {
+      console.error(error);
+      return error.response;
+    }
   };
 
   const logout = () => {
-    user.value = null;
     token.value = null;
+    userEmail.value = null;
+    username.value = null;
     isLoggedIn.value = false;
 
     sessionStorage.removeItem("userToken");
     sessionStorage.removeItem("isLoggedIn");
+    sessionStorage.removeItem("userEmail");
+    sessionStorage.removeItem("username");
   };
 
-  return { user, isLoggedIn, token, login, logout };
+  return { isLoggedIn, token, login, logout, signup, username, userEmail };
 });
--- final-pjt-front/src/views/LoginView.vue
+++ final-pjt-front/src/views/LoginView.vue
@@ -3,23 +3,10 @@
     <h1>로그인</h1>
     <form @submit.prevent="handleLogin">
       <label for="email">이메일</label>
-      <input
-        type="text"
-        id="email"
-        class="user-input"
-        v-model="emailInput"
-        placeholder="이메일을 입력해 주세요."
-      />
+      <input type="text" id="email" class="user-input" v-model="emailInput" placeholder="이메일을 입력해 주세요." />
       <label for="password">비밀번호</label>
-      <input
-        type="password"
-        id="password"
-        class="user-input"
-        v-model="pwInput"
-        placeholder="비밀번호를 입력해 주세요."
-      />
+      <input type="password" id="password" class="user-input" v-model="pwInput" placeholder="비밀번호를 입력해 주세요." />
       <Button content="로그인" ariaLabel="로그인" type="submit"></Button>
-      <!-- <button type="submit">로그인</button> -->
       <p v-if="!isValid">아이디와 비밀번호를 다시 확인해 주세요.</p>
     </form>
   </div>
@@ -32,34 +19,20 @@ import { ref } from "vue";
 import axiosInstance from "@/plugins/axios";
 import { useRouter } from "vue-router";
 
-const userStore = useUserStore();
+const store = useUserStore();
 const router = useRouter();
 const emailInput = ref("");
 const pwInput = ref("");
-const isValid = ref("");
+const isValid = ref(true);
 
 const handleLogin = async () => {
-  try {
-    const userData = {
-      email: emailInput.value,
-      password: pwInput.value,
-    };
-    const response = await axiosInstance.post(
-      `${import.meta.env.VITE_BASE_URL}/accounts/login/`,
-      userData
-    );
-
-    if (response.status === 200) {
-      const userToken = response.data.key;
-      userStore.login(userData, userToken);
-      router.push({ name: "home" });
-    }
-  } catch (err) {
-    console.log(err.response.status);
-    if (err.response.status) {
-      isValid.value = false;
-    }
-    throw new Error(err);
+  const userData = {
+    email: emailInput.value,
+    password: pwInput.value,
+  };
+  const errMsg = store.login(userData);
+  if (errMsg) {
+    isValid.value = false;
   }
 };
 </script>
