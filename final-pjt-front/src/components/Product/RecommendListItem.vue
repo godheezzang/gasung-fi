d0df96dfa9c4d8c22f32de11dc0d23b5f9229260
박가희 <930_10@naver.com>
2024-11-25 01:47:46 +0900

    Feat: 상품 추천 완료

--- final-pjt-front/src/components/Product/RecommendListItem.vue
+++ final-pjt-front/src/components/Product/RecommendListItem.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <!-- {{ product }} -->
-    <p>상품명: {{ product.fin_prdt_nm }}</p>
-    <p>상품 타입: {{ product.product_type }}</p>
-    <p>은행: {{ product.kor_co_nm }}</p>
-    <p>이 상품을 가입한 사람들의 수: {{ product.count }}명</p>
+    <RouterLink :to="{ name: 'product_detail', params: { product_id: product.fin_prdt_cd } }">
+      <p>상품명: {{ product.fin_prdt_nm }}</p>
+      <p>상품 타입: {{ product.product_type }}</p>
+      <p>은행: {{ product.kor_co_nm }}</p>
+      <p>이 상품을 가입한 사람들의 수: {{ product.count }}명</p>
+    </RouterLink>
   </div>
 </template>
 
 <script setup>
+import { RouterLink } from "vue-router";
+
 const props = defineProps({
   product: Object,
 });
