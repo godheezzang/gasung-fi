579972c3526d4b2081e4566ab6a424de8a32db06
박가희 <930_10@naver.com>
2024-11-24 22:05:27 +0900

    Feat: 상품 찜하기 기능

--- final-pjt-front/src/components/Product/ProductDetail.vue
+++ final-pjt-front/src/components/Product/ProductDetail.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1>상품 상세 페이지</h1>
-    <Button content="가입하기" :onClick="handleJoin" ariaLabel="가입하기" />
-    <Button v-if="isStaff" content="금리 수정" :onClick="handleUpdateProduct" ariaLabel="금리 수정" />
+    <Button :content="alreadyJoined ? '찜 완료' : '찜하기'" :onClick="alreadyJoined ? null : handleJoin" ariaLabel="찜하기" :customClass="alreadyJoined ? 'disabled-btn' : ''" />
+    <Button v-if="store.isStaff" content="금리 수정" :onClick="handleUpdateProduct" ariaLabel="금리 수정" />
     <div v-if="product">
       <p>공시 제출월: {{ product.dcls_month }}</p>
       <p>금융 회사명: {{ product.kor_co_nm }}</p>
@@ -20,7 +20,7 @@
 import Button from "@/components/Common/Button.vue";
 import { useUserStore } from "@/stores/user";
 import axios from "axios";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
@@ -30,27 +30,43 @@ const router = useRouter();
 const finPrdtCd = route.params.product_id;
 const productType = route.query.product_type;
 const product = ref({});
+const alreadyJoined = ref(false);
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
-const SAVING_URL = `${BASE_URL}/products/installment_savings/${finPrdtCd}/`;
-const DEPOSIT_URL = `${BASE_URL}/products/deposit/${finPrdtCd}/`;
-const isStaff = ref(false);
 
 // productType에 따라 요청 url 선택하는 로직
 const getUrl = () => {
-  return productType === "saving" ? `${BASE_URL}/products/installment_savings/${finPrdtCd}/` : `${BASE_URL}/products/deposit/${finPrdtCd}`;
+  return productType === "saving" ? `${BASE_URL}/products/installment_savings/${finPrdtCd}/` : `${BASE_URL}/products/deposit/${finPrdtCd}/`;
 };
 
-// TODO: 상품 가입 테스트 필요
+console.log(store.userProducts);
+
+// const productToJoin = {
+//   id: finPrdtCd, // 가입할 상품 ID
+// };
+
+// 이미 가입한 상품 목록에서 해당 상품이 있는지 확인
+const checkAlreadyJoined = () => {
+  alreadyJoined.value = store.userProducts.some((userProduct) => userProduct.fin_prdt_cd === finPrdtCd);
+};
+
 const handleJoin = async () => {
   if (!store.isLoggedIn) {
-    const confirmed = window.confirm("로그인된 사용자만 이용할 수 있어요! \n 로그인 화면으로 이동하시겠어요?");
+    const confirmed = window.confirm("로그인된 사용자만 이용할 수 있어요!\n 로그인 화면으로 이동하시겠어요?");
 
     if (confirmed) {
       router.push({ name: "login" });
     }
+    return;
   }
+
+  if (alreadyJoined.value) {
+    return;
+  }
+
   const url = getUrl();
+  // console.log(url);
+
   try {
     const response = await axios({
       method: "post",
@@ -60,10 +76,14 @@ const handleJoin = async () => {
       },
     });
 
-    console.log(response.data);
+    // console.log(response.data);
+    // 상품 가입 후 사용자 상품 목록 요청
+    store.getUserProducts();
+    alreadyJoined.value = true;
     alert("상품 가입이 완료되었습니다.");
   } catch (error) {
     console.error(error);
+    alert("상품 가입에 실패했어요!");
   }
 };
 
@@ -87,7 +107,7 @@ const fetchProductDetail = async () => {
       url,
     });
 
-    console.log(response.data);
+    // console.log(response.data);
     product.value = {
       ...response.data,
       join_deny_desc: changeJoinDenyDesc(Number(response.data.join_deny)),
@@ -99,6 +119,14 @@ const fetchProductDetail = async () => {
 onMounted(() => {
   fetchProductDetail();
 });
+
+watch(
+  () => store.isStaff,
+  (newValue) => {
+    console.log(newValue);
+    console.log(store.isStaff);
+  }
+);
 </script>
 
 <style scoped></style>
