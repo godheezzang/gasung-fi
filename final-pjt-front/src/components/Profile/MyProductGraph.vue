e89f03831d5107e5f4092dc36d1bdf853d6de961
박가희 <930_10@naver.com>
2024-11-25 01:39:33 +0900

    Wip: 내가 찜한 상품 금리 비교 그래프 진행중

--- final-pjt-front/src/components/Profile/MyProductGraph.vue
+++ final-pjt-front/src/components/Profile/MyProductGraph.vue
@@ -1,7 +1,45 @@
 <template>
-  <div>MyProductGraph</div>
+  <div class="modal" v-if="isVisible">
+    <div class="modal-content">
+      <span class="close" @click="closeModal">&times;</span>
+      <h1>상품 그래프</h1>
+      {{ productDetail.fin_prdt_nm }}
+      <!-- 추가적인 그래프 관련 내용 -->
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { defineProps } from "vue";
+// TODO: 그래프 그리기
+const props = defineProps({
+  productDetail: Object,
+  isVisible: Boolean,
+  closeModal: Function,
+});
+</script>
 
-<style scoped></style>
+<style scoped>
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+.close {
+  cursor: pointer;
+  float: right;
+}
+</style>
