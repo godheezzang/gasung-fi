9805872980d943486caa160fabb3cddfc2cca107
박가희 <930_10@naver.com>
2024-11-25 17:56:39 +0900

    Refactor: 지도 검색 버튼 MapView로 이동

--- final-pjt-front/src/views/MapView.vue
+++ final-pjt-front/src/views/MapView.vue
@@ -1,31 +1,49 @@
 <template>
-  <div class="map-size">
-    <div>
-      <!-- 도/시 선택 -->
-      <label for="province">도/시:</label>
-      <select id="province" v-model="province" @change="updateCities">
-        <option value="" disabled>선택하세요</option>
-        <option v-for="prov in provinces" :key="prov" :value="prov">{{ prov }}</option>
-      </select>
+  <div class="map-size main">
+    <div class="select-container">
+      <div>
+        <div class="select-box">
+          <!-- 도/시 선택 -->
+          <label for="province">도/시 선택</label>
+          <select
+            class="custom-select"
+            id="province"
+            v-model="province"
+            @change="updateCities"
+          >
+            <option value="" disabled>선택하세요</option>
+            <option v-for="prov in provinces" :key="prov" :value="prov">
+              {{ prov }}
+            </option>
+          </select>
+        </div>
+        <div class="select-box">
+          <!-- 시/군/구 선택 -->
+          <label for="city">시/군/구 선택</label>
+          <select class="custom-select" id="city" v-model="city">
+            <option value="" disabled>선택하세요</option>
+            <option v-for="ct in cities" :key="ct" :value="ct">{{ ct }}</option>
+          </select>
+        </div>
+        <div class="select-box">
+          <!-- 은행 선택 -->
+          <label for="bank">은행 선택</label>
+          <select class="custom-select" id="bank" v-model="bank">
+            <option value="" disabled>선택하세요</option>
+            <option v-for="bnk in banks" :key="bnk" :value="bnk">
+              {{ bnk }}
+            </option>
+          </select>
+        </div>
+      </div>
+      <div class="map-button-container">
+        <button @click="searchOnMap">검색하기</button>
+        <button @click="getCurrentLocation">내 주변 은행</button>
+      </div>
     </div>
-    <div>
-      <!-- 시/군/구 선택 -->
-      <label for="city">시/군/구:</label>
-      <select id="city" v-model="city">
-        <option value="" disabled>선택하세요</option>
-        <option v-for="ct in cities" :key="ct" :value="ct">{{ ct }}</option>
-      </select>
-    </div>
-    <div>
-      <!-- 은행 선택 -->
-      <label for="bank">은행:</label>
-      <select id="bank" v-model="bank">
-        <option value="" disabled>선택하세요</option>
-        <option v-for="bnk in banks" :key="bnk" :value="bnk">{{ bnk }}</option>
-      </select>
-    </div>
+
     <!-- 지도 출력 Component 연결, 선택된 데이터 전달 -->
-    <MapSearch :province="province" :city="city" :bank="bank" />
+    <MapSearch ref="mapSearch" :province="province" :city="city" :bank="bank" />
   </div>
 </template>
 
@@ -40,6 +58,7 @@ const infos = store.infos;
 const banks = store.banks;
 const cities = ref([]);
 const selectLocation = ref("");
+const mapSearch = ref(null);
 
 // 도/시 목록
 const provinces = computed(() => infos.map((info) => info.prov));
@@ -57,7 +76,60 @@ const updateCities = () => {
   cities.value = selectedInfo ? selectedInfo.city : [];
 };
 
+const searchOnMap = () => {
+  if (mapSearch.value) {
+    mapSearch.value.searchOnMap();
+  }
+};
+
+const getCurrentLocation = () => {
+  if (mapSearch.value) {
+    mapSearch.value.getCurrentLocation();
+  }
+};
+
 watch(province, () => {
   updateCities();
 });
 </script>
+
+<style scoped>
+.map-size {
+  display: flex;
+  align-items: center;
+}
+.select-container {
+  margin-top: 1rem;
+  flex-grow: 0;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.select-container label {
+  font-weight: var(--font-weight-medium);
+}
+
+.select-box {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2rem;
+  gap: 0.3rem;
+}
+
+.map-button-container {
+  margin: 1rem 0.5rem;
+  display: flex;
+}
+
+.map-button-container button {
+  display: block;
+  margin: 1rem 0 1rem 1rem;
+  background-color: var(--color-black);
+  color: var(--color-white);
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+  width: 10rem;
+}
+</style>
