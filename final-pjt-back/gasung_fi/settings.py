81f801a9830dd172c39bfaaa2383751240d3e597
hjkim2040 <hjkim2661@gmail.com>
2024-11-25 20:25:59 +0900

    Fix: CORS_ALLOWED 추가

--- final-pjt-back/gasung_fi/settings.py
+++ final-pjt-back/gasung_fi/settings.py
@@ -50,6 +50,7 @@ ACCOUNT_AUTHENTICATION_REDIRECTS = False
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
+    'http://43.200.128.146',
 ]
 
 SOCIALACCOUNT_PROVIDERS ={
