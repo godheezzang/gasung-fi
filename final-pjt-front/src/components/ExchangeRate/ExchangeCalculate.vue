1237e77cee05a491fa2be2128d28da6518edfa8c
hjkim2040 <hjkim2661@gmail.com>
2024-11-26 03:53:07 +0900

    Add: 환율 변환 기능

--- final-pjt-front/src/components/ExchangeRate/ExchangeCalculate.vue
+++ final-pjt-front/src/components/ExchangeRate/ExchangeCalculate.vue
@@ -12,22 +12,26 @@
           {{ country.country }}
         </option>
       </select>
-      <input type="number" />
+
     </div>
 
     <div>
-      <label for="koreanMoney">한국 원화:</label>
+      <label for="koreanMoney">한국 원화 : </label>
       <input
         type="text"
         id="koreanMoney"
         v-model.number="kMoneyInput"
-        @input="convertMoney"
+        @input="convertToForeign"
       />
-      원
     </div>
 
     <div v-if="convertedAmount !== null">
-      <p>변환된 금액: {{ convertedAmount }} {{ selectedCountry?.money }}</p>
+      <label for="foreignMoney">{{selectedCountry.country}} {{selectedCountry.money}} : </label>
+      <input type="text"
+             id="foreignMoney"
+             v-model.number="foreignMoneyInput"
+             @input="convertToKorean"
+      />
     </div>
     <div v-else>
       <p>환율 정보를 선택하세요.</p>
@@ -41,8 +45,9 @@ import { onMounted, ref, computed, watch } from "vue";
 
 const store = useRateStore();
 const selectedCountry = ref(null);
-const convertedAmount = ref(1);
+const convertedAmount = ref(null);
 const kMoneyInput = ref("");
+const foreignMoneyInput = ref("");
 
 // 국가 리스트를 formattedRates에서 추출
 const countries = computed(() => {
@@ -59,23 +64,57 @@ console.log(selectedCountry.value);
 // const kMoneyInput = ref(selectedCountry.value.bkpr);
 
 // 환율 변환 함수
-const convertMoney = () => {
+const convertToForeign = () => {
   if (selectedCountry.value && !isNaN(kMoneyInput.value)) {
     const bkprStr = selectedCountry.value.bkpr; // 선택된 국가의 bkpr
     const bkpr = Number(bkprStr.replace(/,/g, "")); // 쉼표 제거 및 숫자로 변환
 
     if (!isNaN(bkpr)) {
       convertedAmount.value = (kMoneyInput.value / bkpr).toFixed(2); // 변환된 금액 계산
+      foreignMoneyInput.value = convertedAmount.value; // 외국 통화 입력 필드 업데이트
     } else {
       convertedAmount.value = null; // bkpr가 유효하지 않은 경우
     }
   } else {
     convertedAmount.value = null; // 국가가 선택되지 않았거나 입력이 유효하지 않은 경우
+    foreignMoneyInput.value = 0; // 외국 통화 입력 필드 초기화
   }
 };
+const convertToKorean = () => {
+  if (selectedCountry.value && !isNaN(foreignMoneyInput.value)) {
+    const bkprStr = selectedCountry.value.bkpr; // 선택된 국가의 bkpr
+    const bkpr = Number(bkprStr.replace(/,/g, "")); // 쉼표 제거 및 숫자로 변환
+
+    if (!isNaN(bkpr)) {
+      kMoneyInput.value = (foreignMoneyInput.value * bkpr).toFixed(2); // 변환된 금액 계산
+    } else {
+      kMoneyInput.value = 0; // bkpr가 유효하지 않은 경우
+    }
+  }
+  else {
+    kMoneyInput.value = 0; // 국가가 선택되지 않았거나 입력이 유효하지 않은 경우
+  }
+};
+// const convertMoney = () => {
+//   if (selectedCountry.value && !isNaN(kMoneyInput.value)) {
+//     const bkprStr = selectedCountry.value.bkpr; // 선택된 국가의 bkpr
+//     const bkpr = Number(bkprStr.replace(/,/g, "")); // 쉼표 제거 및 숫자로 변환
+//
+//     if (!isNaN(bkpr)) {
+//       convertedAmount.value = (kMoneyInput.value / bkpr).toFixed(2); // 변환된 금액 계산
+//     } else {
+//       convertedAmount.value = null; // bkpr가 유효하지 않은 경우
+//     }
+//   } else {
+//     convertedAmount.value = null; // 국가가 선택되지 않았거나 입력이 유효하지 않은 경우
+//   }
+// };
 
 // 선택된 국가가 변경될 때마다 변환 계산
-watch(selectedCountry, convertMoney);
+watch(selectedCountry, () => {
+  convertToForeign();
+  convertToKorean();
+});
 
 const fetchRate = async () => {
   await store.getRate();
