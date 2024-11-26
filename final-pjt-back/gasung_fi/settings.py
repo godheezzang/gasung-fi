b75f1fc0c0cc788a293de76ab2b27f1763600a55
hjkim2040 <hjkim2661@gmail.com>
2024-11-27 04:27:43 +0900

    Fix: settings.py 수정

--- final-pjt-back/gasung_fi/settings.py
+++ final-pjt-back/gasung_fi/settings.py
@@ -206,4 +206,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MIDEA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
