1fb6802be8a1dce86f039a2a82c5b4cf70e0a60b
박가희 <930_10@naver.com>
2024-11-26 15:32:06 +0900

    Style: 댓글 목록 & 생성 컴포넌트 스타일링

--- final-pjt-front/src/components/Community/CommentCreate.vue
+++ final-pjt-front/src/components/Community/CommentCreate.vue
@@ -1,13 +1,52 @@
 <template>
   <div>
-    <form @submit.prevent="isEdit ? handleUpdate() : handleCreate()" class="comment-form">
-      <Textarea placeholder="250자 이내의 댓글을 달아주세요. 욕설 및 비방은 삭제 조치됩니다." id="reply" v-model="commentInput" />
+    <form
+      @submit.prevent="isEdit ? handleUpdate() : handleCreate()"
+      class="comment-form"
+    >
+      <Textarea
+        placeholder="250자 이내의 댓글을 달아주세요. 욕설 및 비방은 삭제 조치됩니다."
+        id="reply"
+        v-model="commentInput"
+      />
       <button type="submit">작성</button>
     </form>
+    <p class="err-msg" v-if="!isValid">댓글 내용을 작성해 주세요.</p>
+    <p class="err-msg" v-if="commentInput.length > 250">
+      댓글 작성은 250자까지만 가능합니다.
+    </p>
+  </div>
+  <div>
+    <Teleport to="body">
+      <Modal :show="showModal" @close="showModal = false">
+        <template #header>
+          <h3>로그인 필요</h3>
+        </template>
+        <template #body>
+          <p>로그인이 필요한 서비스입니다.</p>
+          <p>로그인하시고 이용해보세요!</p>
+        </template>
+        <template #footer>
+          <button
+            @click="
+              () => {
+                console.log('Redirect button clicked');
+                redirectToLogin();
+              }
+            "
+          >
+            확인
+          </button>
+
+          <button @click="showModal = false">취소</button>
+        </template>
+      </Modal>
+    </Teleport>
   </div>
 </template>
 
 <script setup>
+import Modal from "@/components/Common/Modal.vue";
 import Textarea from "@/components/Common/Textarea.vue";
 import { useUserStore } from "@/stores/user";
 import axios from "axios";
@@ -19,13 +58,16 @@ const props = defineProps({
   parentId: Number,
 });
 
+const emit = defineEmits(["close", "commentCreated"]);
 const commentInput = ref("");
 const route = useRoute();
 const articleId = route.params.article_id;
 const store = useUserStore();
-// const emit = defineEmits(["close"]);
 const isEdit = ref(false);
 const router = useRouter();
+const showModal = ref(false);
+const isValid = ref(true);
+const userStore = useUserStore();
 
 // TODO: 댓글 수정
 
@@ -42,9 +84,33 @@ const router = useRouter();
 //   }
 // });
 
+const redirectToLogin = () => {
+  console.log("log");
+  router.push({ name: "login" }); // 로그인 페이지로 이동
+  commentInput.value = "";
+  showModal.value = false; // 모달 닫기
+};
+
 const handleCreate = async () => {
+  if (!store.isLoggedIn) {
+    showModal.value = true;
+    return;
+  }
+
+  if (commentInput.value.length > 250) {
+    return;
+  }
+
+  if (commentInput.value.trim() === "") {
+    isValid.value = false;
+    return;
+  }
   try {
-    const url = props.isReply ? `${import.meta.env.VITE_BASE_URL}/articles/${articleId}/comments/${props.parentId}/` : `${import.meta.env.VITE_BASE_URL}/articles/${articleId}/comments/`;
+    const url = props.isReply
+      ? `${import.meta.env.VITE_BASE_URL}/articles/${articleId}/comments/${
+          props.parentId
+        }/`
+      : `${import.meta.env.VITE_BASE_URL}/articles/${articleId}/comments/`;
 
     const response = await axios.post(
       url,
@@ -57,9 +123,15 @@ const handleCreate = async () => {
     );
 
     console.log(response);
-    if (response.status === 200) {
+    if (response.status === 201) {
+      const newComment = {
+        ...response.data,
+        username: userStore.username,
+      };
       commentInput.value = "";
-      // emit("close");
+      emit("close");
+      emit("commentCreated", newComment);
+      router.go(0);
     }
   } catch (error) {
     console.error(error);
@@ -73,7 +145,7 @@ const handleUpdate = () => {};
 .comment-form {
   display: flex;
   gap: 1rem;
-  padding: 1rem;
+  padding-top: 1rem;
 }
 
 .comment-form button {
--- final-pjt-front/src/components/Community/CommentListItem.vue
+++ final-pjt-front/src/components/Community/CommentListItem.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="comment-container">
-    <!-- {{ comment }} -->
-    <p>{{ comment.username }}</p>
-    <p>{{ comment.content }}</p>
-    <button @click="handleDelete" v-if="isCurrentUser">삭제</button>
-    <button @click="toggleReply" v-if="!isComment">
-      {{ isReply ? "답글 취소" : "답글" }}
-    </button>
-    <button @click="handleUpdate" v-if="isCurrentUser">수정</button>
+    <p class="comment-username">{{ comment.username }}</p>
+    <p class="comment-content">{{ comment.content }}</p>
+    <div class="comment-btns">
+      <button @click="handleDelete" v-if="isCurrentUser">삭제</button>
+      <button @click="toggleReply" v-if="!isComment">
+        {{ isReply ? "답글 취소" : "답글" }}
+      </button>
+      <button @click="handleUpdate" v-if="isCurrentUser">수정</button>
+    </div>
 
     <div v-if="comment.replies" class="reply-container">
-      <CommentListItem v-for="reply in comment.replies" :key="reply.id" :comment="reply" :isComment="true" />
+      <CommentListItem
+        v-for="reply in comment.replies"
+        :key="reply.id"
+        :comment="reply"
+        :isComment="true"
+      />
     </div>
-    <CommentCreate v-if="isReply" :isReply="true" :parentId="comment.id" @close="isReply = false" class="reply-container" />
+    <CommentCreate
+      v-if="isReply"
+      :isReply="true"
+      :parentId="comment.id"
+      @close="isReply = false"
+      @commentCreated="handleCommentCreated"
+      class="reply-container"
+    />
   </div>
 </template>
 
@@ -34,26 +47,19 @@ const props = defineProps({
 
 const route = useRoute();
 const store = useUserStore();
-
 const articleId = route.params.article_id;
 const isReply = ref(false);
 
-const CommentListItem = defineAsyncComponent(() => import("@/components/Community/CommentListItem.vue"));
+const CommentListItem = defineAsyncComponent(() =>
+  import("@/components/Community/CommentListItem.vue")
+);
 
-// console.log(props.comment);
-
-const userEmail = computed(() => sessionStorage.getItem("userEmail").replace(/"/g, ""));
+const userEmail = computed(() =>
+  sessionStorage.getItem("userEmail").replace(/"/g, "")
+);
 const isCurrentUser = computed(() => {
-  // console.log("userEmail:", userEmail.value);
-  // console.log("commentUser:", String(props.comment.email));
-
-  // console.log(typeof userEmail.value);
-  // console.log(typeof props.comment.email);
-
   return String(userEmail.value) === String(props.comment.email);
 });
-
-console.log("isCurrentUser:", isCurrentUser.value);
 
 const toggleReply = () => {
   isReply.value = !isReply.value;
@@ -61,13 +67,21 @@ const toggleReply = () => {
 
 const handleDelete = async () => {
   try {
-    const response = await axios.delete(`${import.meta.env.VITE_BASE_URL}/articles/${articleId}/comments/${props.comment.id}`, {
-      headers: {
-        Authorization: `Token ${store.token}`,
-      },
-    });
+    const response = await axios.delete(
+      `${import.meta.env.VITE_BASE_URL}/articles/${articleId}/comments/${
+        props.comment.id
+      }/`,
+      {
+        headers: {
+          Authorization: `Token ${store.token}`,
+        },
+      }
+    );
 
     console.log(response);
+    if (response.status === 204) {
+      emit("commentDeleted", props.comment.id);
+    }
   } catch (error) {}
 };
 
@@ -81,5 +95,53 @@ const handleUpdate = async () => {
 <style scoped>
 .reply-container {
   margin-left: 2rem;
+}
+
+.comment-container {
+  width: 100%;
+  /* padding: 0 1rem; */
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1rem;
+  background-color: var(--color-spinner-blue);
+  padding: 1rem;
+  border-radius: 10px;
+}
+
+.comment-username {
+  font-weight: var(--font-weight-medium);
+  font-size: var(--font-size-mlarge);
+  color: var(--color-deep-blue);
+  letter-spacing: -1px;
+}
+
+.comment-content {
+  overflow: hidden;
+  white-space: normal;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 100;
+  line-clamp: 100;
+  word-break: break-word;
+}
+
+.comment-btns {
+  margin-top: 0.5rem;
+  display: flex;
+  gap: 0.5rem;
+}
+
+.comment-btns button {
+  background-color: var(--color-black);
+  border-radius: 5px;
+  font-size: var(--font-size-extra-small);
+  padding: 0.5rem;
+  color: var(--color-white);
+}
+
+.comment-btns button:hover {
+  background-color: var(--color-gray-03);
 }
 </style>
