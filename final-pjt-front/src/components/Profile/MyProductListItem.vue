db03362cb6e72cb11cb8f7d4916ca949afe12496
박가희 <930_10@naver.com>
2024-11-27 10:44:23 +0900

    Style: 은행 이미지 추가

--- final-pjt-front/src/components/Profile/MyProductListItem.vue
+++ final-pjt-front/src/components/Profile/MyProductListItem.vue
@@ -1,17 +1,27 @@
 <template>
   <div @click="openModal" class="product-card">
-    <p>상품명: {{ product.fin_prdt_nm }}</p>
-    <p>상품 타입: {{ product.product_type }}</p>
-    <p>은행: {{ product.kor_co_nm }}</p>
-    <p>찜한 날짜: {{ formatDate(product.created_at) }}</p>
+    <p class="mypt-title">{{ product.fin_prdt_nm }}</p>
+    <p class="mypt-type" :class="{ deposit: isDeposit }">
+      {{ product.product_type }}
+    </p>
+    <div class="mypt-bank">
+      <img :src="imgPath" alt="은행 이미지" class="product-img" />
+      <p class="bankname">{{ product.kor_co_nm }}</p>
+    </div>
+    <p class="mypt-date">찜 날짜 | {{ formatDate(product.created_at) }}</p>
   </div>
-  <MyProductGraph :productDetail="productDetail" :isVisible="isVisible" :closeModal="closeModal" />
+  <MyProductGraph
+    :productDetail="productDetail"
+    :isVisible="isVisible"
+    :closeModal="closeModal"
+  />
 </template>
 
 <script setup>
 import MyProductGraph from "@/components/Profile/MyProductGraph.vue";
+import { useBankStore } from "@/stores/bank";
 import axios from "axios";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 
 const props = defineProps({
   product: Object,
@@ -21,7 +31,18 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 const finPrdtCd = props.product.fin_prdt_cd;
 const productDetail = ref({});
 const isVisible = ref(false);
+const isDeposit = ref(false);
+const bankStore = useBankStore();
+const bankName = props.product.kor_co_nm;
+const imgPath = bankStore.bankImage[bankName];
 
+watch(
+  () => props.product.product_type,
+  (newType) => {
+    isDeposit.value = newType === "정기 예금";
+  }
+);
+isDeposit.value = props.product.product_type === "정기 예금";
 const openModal = () => {
   isVisible.value = true;
 };
@@ -41,7 +62,9 @@ const formatDate = (dateString) => {
 
 // productType에 따라 요청 url 선택하는 로직
 const getUrl = () => {
-  return props.product.product_type === "정기 적금" ? `${BASE_URL}/products/installment_savings/${finPrdtCd}/` : `${BASE_URL}/products/deposit/${finPrdtCd}/`;
+  return props.product.product_type === "정기 적금"
+    ? `${BASE_URL}/products/installment_savings/${finPrdtCd}/`
+    : `${BASE_URL}/products/deposit/${finPrdtCd}/`;
 };
 
 const fetchProductData = async () => {
@@ -63,4 +86,49 @@ onMounted(async () => {
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.product-card {
+  cursor: pointer;
+}
+
+.product-card:hover {
+  box-shadow: 0 0 10px var(--color-primary-blue);
+}
+
+.mypt-title {
+  font-size: var(--font-size-mlarge);
+  font-weight: var(--font-weight-medium);
+}
+
+.mypt-type {
+  background-color: var(--color-primary-blue);
+  color: var(--color-white);
+  font-weight: var(--font-weight-medium);
+  padding: 0.3rem 0.6rem;
+  border-radius: 20px;
+  font-size: var(--font-size-small);
+}
+
+.deposit {
+  background-color: var(--color-primary-beige);
+  color: var(--color-black);
+}
+
+.mypt-bank {
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+}
+
+.product-img {
+  width: 3.5rem;
+  margin-right: 0.5rem;
+  margin-left: -3rem;
+  border-radius: 50%;
+}
+
+.bankname {
+  font-weight: var(--font-weight-medium);
+}
+</style>
